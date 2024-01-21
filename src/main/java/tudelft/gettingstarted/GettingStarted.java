5bc142a2d090990ca8283ae1d7811087fe8be8b2
Paco <fran_21079@hotmail.com>
2024-01-21 21:32:13 +0100

    test(junit): testing get started

--- src/main/java/tudelft/gettingstarted/GettingStarted.java
+++ src/main/java/tudelft/gettingstarted/GettingStarted.java
@@ -1,7 +1,12 @@
 package tudelft.gettingstarted;
 
+import org.testng.annotations.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class GettingStarted {
     public int addFive (int number) {
         return number + 5;
     }
 }
+
+
